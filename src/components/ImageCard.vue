cb1dc084b6b509bd136bc5c2b7050f6ff3657d6d
Xolof <olaf.sebastian.johansson@gmail.com>
2022-09-19 19:29:45 +0200

    Create the ImageCard component.
    The image object coms in as a prop. The image is then rendered in the template. There's a text informing the user that the image is loading, which is removed when the image load event is triggered.

---
+++ src/components/ImageCard.vue
@@ -0,0 +1,35 @@
+<template>
+<a :href="image.url">
+    <div
+        v-if="state.isLoading"
+        class="w-11/12 my-0 mx-auto block h-48"
+    >
+        <p >Loading...</p>
+    </div>
+    <img
+        :src="image.thumbnailUrl"
+        :alt="image.title"
+        class="w-11/12 my-0 mx-auto block"
+        @load="setIsLoaded()"
+    />
+</a>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue"
+
+const props = defineProps({
+    image: {
+        type: Object,
+        required: true
+    }
+})
+
+const state = ref({
+    isLoading: true
+})
+
+function setIsLoaded () {
+    state.value.isLoading = false
+}
+</script>
