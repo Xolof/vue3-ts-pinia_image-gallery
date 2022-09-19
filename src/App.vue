3cc12abb0a35a20a9030903b95d8a10427419034
Xolof <olaf.sebastian.johansson@gmail.com>
2022-09-19 19:16:00 +0200

    setup the App.vue with the Gallery in the template.
    The store isn't used yet.

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,22 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { useImageStore } from "./stores/useImageStore"
+import Gallery from "./components/Gallery.vue"
+
+const store = useImageStore()
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+    <p class="text-3xl text-gray-700 text-center font-bold mb-5">
+      Image Gallery
+    </p>
+    <Suspense>
+      <Gallery />
+    </Suspense>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
