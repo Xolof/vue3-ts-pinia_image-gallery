f16eb840cc8cb9a993406170bfcac825ed3275cd
Xolof <olaf.sebastian.johansson@gmail.com>
2022-09-21 17:46:21 +0200

    Show the large version of the image when the thumbnail is clicked.

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,11 +1,30 @@
 <template>
   <section class="flex flex-wrap -mx-2 .justify-evenly">
     <div
+      v-if="state.largeImage"
+      @click="hideLargeImage()"
+      class="bg-white absolute cursor-pointer left-0 right-0 flex items-center justify-center h-screen"
+      v-bind:style="{
+        top: state.pageYOffset + 'px',
+        bottom: '-' + state.pageYOffset + state.innerHeight + 'px',
+      }"
+    >
+      <div>
+        <img :src="state.largeImage.url" :alt="state.largeImage.title" />
+        <p>{{ state.largeImage.title }}</p>
+      </div>
+    </div>
+    <div
       v-for="image in state.images"
       :key="image.id"
       class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 px2"
     >
-      <ImageCard :image="image" />
+      <div
+        @click="showLargeImage(image.url, image.title)"
+        class="cursor-pointer"
+      >
+        <ImageCard :image="image" />
+      </div>
     </div>
   </section>
   <button
@@ -36,12 +55,16 @@ const state = reactive<{
   limit: number;
   images: Image[];
   errorMessage: string;
-  loading: boolean;
+  largeImage: any;
+  pageYOffset: number;
+  innerHeight: number;
 }>({
   limit: 6,
   images: [],
   errorMessage: store.errorMessage,
-  loading: true,
+  largeImage: false,
+  pageYOffset: window.pageYOffset,
+  innerHeight: window.innerHeight,
 });
 
 const imageData = store.getImages;
@@ -50,5 +73,20 @@ state.images = imageData.slice(0, state.limit);
 function increaseLimit() {
   state.limit += 6;
   state.images = imageData.slice(0, state.limit);
+}
+
+function showLargeImage(url: string, title: string) {
+  document.documentElement.style.overflow = "hidden";
+  state.pageYOffset = window.pageYOffset;
+  state.innerHeight = window.innerHeight;
+  state.largeImage = {
+    url,
+    title,
+  };
+}
+
+function hideLargeImage() {
+  document.documentElement.style.overflow = "auto";
+  state.largeImage = false;
 }
 </script>
