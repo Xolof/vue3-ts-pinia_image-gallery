161e26891ae6b7022636077348fe9f53a2fc9b08
Xolof <olaf.sebastian.johansson@gmail.com>
2022-09-21 17:25:49 +0200

    Rename imagestore file. Simplify the store and improve error handling.

---
+++ src/stores/imagestore.ts
@@ -0,0 +1,44 @@
+import { defineStore } from "pinia";
+
+type Image = {
+  albumId: number;
+  id: number;
+  url: string;
+  thumbnailUrl: string;
+  title: string;
+};
+
+export const useImageStore = defineStore(
+  "images", {
+  state: () => ({
+    /**
+     * @type {Image[]}
+     */
+    images: [],
+    /**
+     * @type {String}
+     */
+     errorMessage: "",
+    /**
+     * @type {Boolean}
+     */
+     message: "",
+  }),
+  actions: {
+    async fetchImageData() {
+      try {
+        this.message = "Loading..."
+        const res = await fetch("https://jsonplaceholder.typicode.com/photos");
+        const imageData = await res.json();
+        this.images = imageData;
+        this.message = "";
+      } catch (e) {
+        if (e instanceof Error) {
+          console.error(e);
+          this.errorMessage = e.message;
+          this.message = "";
+        }
+      }
+    }
+  }
+});
