59444a4194b4c91f131986b5fd4a51a2b81911d6
Xolof <olaf.sebastian.johansson@gmail.com>
2022-09-21 21:56:17 +0200

    Add a button for closing the large image overlay.

--- src/components/LargeImageOverLay.vue
+++ src/components/LargeImageOverLay.vue
@@ -1,13 +1,20 @@
 <template>
   <div
     v-if="largeImage"
-    class="bg-slate-50 absolute cursor-pointer left-0 right-0 flex flex-col items-center justify-center h-screen z-50"
+    class="bg-slate-50 absolute left-0 right-0 flex flex-col items-center justify-center h-screen z-50"
     :style="{
       top: pageYOffset + 'px',
       bottom: '-' + pageYOffset + innerHeight + 'px',
     }"
-    @click="handleClick()"
   >
+    <div class="absolute top-0 right-0">
+      <button
+        class="block w-28 h-28 cursor-pointer border-solid border-4 border-gray-600 rounded m-6"
+        @click="handleClick()"
+      >
+        <img src="/public/closeBtn.svg" alt="Close" />
+      </button>
+    </div>
     <div
       v-if="state.isLoading"
       class="w-11/12 my-0 mx-auto block h-[590px] flex items-center justify-center h-screen"
