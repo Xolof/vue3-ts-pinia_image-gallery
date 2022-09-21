2705128a905373cdf16dbcbc97c73327dc8ffe7d
Xolof <olaf.sebastian.johansson@gmail.com>
2022-09-21 20:14:07 +0200

    Add loading message for large image

--- src/components/LargeImageOverLay.vue
+++ src/components/LargeImageOverLay.vue
@@ -1,17 +1,24 @@
 <template>
   <div
     v-if="largeImage"
-    class="bg-white absolute cursor-pointer left-0 right-0 flex items-center justify-center h-screen"
+    class="bg-white absolute cursor-pointer left-0 right-0 flex flex-col items-center justify-center h-screen"
     :style="{
       top: pageYOffset + 'px',
       bottom: '-' + pageYOffset + innerHeight + 'px',
     }"
     @click="handleClick()"
   >
+    <div
+      v-if="state.isLoading"
+      class="w-11/12 my-0 mx-auto block h-[590px] flex items-center justify-center h-screen"
+    >
+      <p class="text-center block">Loading...</p>
+    </div>
     <div class="w-full">
       <img
         :src="largeImage.url"
         :alt="largeImage.title"
+        class="my-0 mx-auto block"
         @load="setIsLoaded()"
       />
       <p class="text-center">{{ largeImage.title }}</p>
