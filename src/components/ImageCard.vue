bb15404581e6d30396533e0aef9466fd3b82c1e3
Xolof <olaf.sebastian.johansson@gmail.com>
2022-09-21 21:05:29 +0200

    Asjust positon of image loading message

--- src/components/ImageCard.vue
+++ src/components/ImageCard.vue
@@ -1,16 +1,20 @@
 <template>
-  <div v-if="state.isLoading" class="w-11/12 my-0 mx-auto block h-56">
-    <p>Loading...</p>
+  <div class="relative">
+    <div v-if="state.isLoading" class="w-11/12 absolute top-0 h-56 text-center">
+      <div class="absolute top-1/3 left-0 right-0">
+        <p>Loading...</p>
+      </div>
+    </div>
+    <img
+      :src="image.thumbnailUrl"
+      :alt="image.title"
+      class="w-11/12 my-0 mx-auto block"
+      width="150"
+      height="150"
+      @load="setIsLoaded()"
+    />
+    <p class="w-11/12 my-0 mx-auto block">{{ image.title }}</p>
   </div>
-  <img
-    :src="image.thumbnailUrl"
-    :alt="image.title"
-    class="w-11/12 my-0 mx-auto block"
-    width="150"
-    height="150"
-    @load="setIsLoaded()"
-  />
-  <p class="w-11/12 my-0 mx-auto block">{{ image.title }}</p>
 </template>
 
 <script setup lang="ts">
