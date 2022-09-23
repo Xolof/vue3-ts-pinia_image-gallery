9f2ae7e0ab4cdd75c5c7ace98f56f694266fde0a
Xolof <olaf.sebastian.johansson@gmail.com>
2022-09-23 18:01:02 +0200

    Change styles and add more info to the image card

--- src/components/ImageCard.vue
+++ src/components/ImageCard.vue
@@ -2,7 +2,7 @@
   <div class="relative">
     <div v-if="state.isLoading" class="w-11/12 absolute top-0 h-56 text-center">
       <div class="absolute top-1/3 left-0 right-0">
-        <p>Loading...</p>
+        <p class="text-gray-800">Loading...</p>
       </div>
     </div>
     <img
@@ -12,7 +12,12 @@
       height="150"
       @load="setIsLoaded()"
     />
-    <p class="w-11/12 my-0 mx-auto block">{{ image.title }}</p>
+    <p class="w-11/12 my-0 mx-auto block text-gray-800">
+      <span class="font-bold">Title:</span> {{ image.title }}
+    </p>
+    <p class="w-11/12 my-0 mx-auto block text-gray-800">
+      <span class="font-bold">Album id:</span> {{ image.albumId }}
+    </p>
   </div>
 </template>
 
